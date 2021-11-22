76432eb4143e436c9581381637791f03782c541c
Almir Jr <almirafjr.dev@gmail.com>
2021-11-22 10:39:38 -0300

    Add third test stage

--- TesteDeAutomacao/NodeJS/test/opentdbSearch.spec.ts
+++ TesteDeAutomacao/NodeJS/test/opentdbSearch.spec.ts
@@ -32,7 +32,7 @@ async function setWebDriver(browser:string, host: string) {
 	if (host.match('dockerhost')) {
 		driver = await new Builder().forBrowser(browser).usingServer('http://selenium-hub:4444/wd/hub').build();
 	} else {
-		driver = await new Builder().forBrowser(browser).usingServer('http://localhost:4444/wd/hub').build();
+		driver = await new Builder().forBrowser(browser).build();
 	}
 }
 
@@ -80,6 +80,39 @@ describe('Searching for "Science: Computers" in "Category" category', () => {
 				throw new Error(`table length is diferent than expected (expected 25 rows but received ${tableCells.length} rows)`);
 			} else {
 				await driver.findElement(By.className('pagination pagination-lg'));
+			}
+
+		} finally {
+			await driver.quit();
+		}
+	});
+});
+
+describe('Searching for "mets11rap" in "User" category', () => {
+	before(async () => {
+		searchText = process.env.WEB_SEARCH_TEXT_THIRD_STAGE;
+		searchCategory = process.env.WEB_SEARCH_CATEGORY_THIRD_STAGE;
+		await setWebDriver(process.env.WEB_BROWSER, process.env.HOST_ENV);
+	});
+	it('should return a website with a table with 24 questions created by the user "mets11rap"', async () => {
+		try {
+			await driver.get(webUrl);
+
+			await (await driver.findElement(By.id('query'))).sendKeys(searchText);
+			await (await driver.findElement(By.id('type'))).sendKeys(searchCategory);
+			await (await driver.findElement(By.className('btn btn-sm btn-default'))).click();
+
+			const tableCells: WebElement[] = await driver.findElements(By.xpath('//*[@id="page-top"]/div[2]/table/tbody/tr'));
+
+			if (tableCells.length !== 24) {
+				throw new Error(`table length is diferent than expected (expected 24 rows but received ${tableCells.length} rows)`);
+			} else {
+				const userProfile: WebElement = await driver.findElement(By.xpath('//*[@id="page-top"]/div[2]/table/tbody/tr[1]/td[6]/a'));
+				const userProfileName = await userProfile.getText();
+
+				if (userProfileName !== 'mets11rap') {
+					throw new Error(`user name is different than expected (expected mets11rap but received ${userProfileName})`);
+				}
 			}
 
 		} finally {
